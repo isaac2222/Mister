8414ccba8e312be48f08473453aa3d59da46cc65
Isaac Philippe <52184718+isaac2222@users.noreply.github.com>
2020-10-12 21:35:07 -0300

    remove template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import {Component, OnInit} from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 
 @Component({
   selector: 'mt-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: [
+    '../assets/css/bootstrap.min.css',
+    '../assets/css/default-css.css',
+    '../assets/css/font-awesome.min.css',
+    '../assets/css/metisMenu.css',
+    '../assets/css/responsive.css',
+    '../assets/css/slicknav.min.css',
+    '../assets/css/styles.css',
+    '../assets/css/themify-icons.css',
+    '../assets/css/typography.css'
+  ]
 })
+
 export class AppComponent implements OnInit {
 
-  content = 'Welcome do Meat App!'
+  content = 'Bem vindo ao Altima FIM'
 
   constructor() { }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'mt-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css',
+  ]
+
 })
 export class HomeComponent implements OnInit {
 
